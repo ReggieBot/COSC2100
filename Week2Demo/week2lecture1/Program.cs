40abf2bbef5c197f7bf48099068f378f89b565c0
ReggieBot <reggiestephenbrown@gmail.com>
2025-09-08 20:01:14 -0400

    Week 2 demo 1

---
+++ Week2Demo/week2lecture1/Program.cs
@@ -0,0 +1,44 @@
+﻿/// Reggie Brown
+/// Prof. Fabian
+/// September 8, 2025
+/// Last Modified - Sepetember 8, 2025
+/// Description - Week 2 Lecture 1
+/// Object-Oriented Programming 2
+
+using System.Numerics;
+
+// store whole numbers
+int age = 50;
+
+// store decimal values (64 bit)
+double gpa = 4.8;
+
+// char
+char grade = 'A';
+
+// boolean
+bool keepGoing = false;
+
+// float type (32 bit)
+float myChequingBalance = 2.43f;
+
+// string
+string myName = "Reggie";
+
+// 64 bit signed
+long myID = 48537578285724;
+
+// 64 bit unsigned
+ulong bigNumber = 4394395734734834598; 
+
+
+// console output
+Console.WriteLine(bigNumber);
+
+
+// method
+static double sumTwoNumbers(int num1, int num2)
+{
+    return num1 + num2; 
+}
+
