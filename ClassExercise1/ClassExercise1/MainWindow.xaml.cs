cb749d20923e8b83b23378e4f452a0f71a0d967a
ReggieBot <reggiestephenbrown@gmail.com>
2025-09-18 16:42:46 -0400

    Finished xaml.cs file. Program in working state

--- ClassExercise1/ClassExercise1/MainWindow.xaml.cs
+++ ClassExercise1/ClassExercise1/MainWindow.xaml.cs
@@ -20,5 +20,64 @@ namespace ClassExercise1
         {
             InitializeComponent();
         }
+
+        // logic for Add button click event
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double num1 = Convert.ToDouble(Input1TextBox.Text);
+                double num2 = Convert.ToDouble(Input2TextBox.Text);
+                double sum = num1 + num2;
+
+                // display result to label (parsed to string)
+                labelResult.Content = sum.ToString();
+            }
+
+            // catch any exceptions and display error message
+            // invalid input (non-numeric) will throw exception
+            catch (Exception ex)
+            {
+                labelResult.Content = "Error" + ex.Message;
+            }
+        }
+
+        // logic for multiply button click event
+        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double num1 = Convert.ToDouble(Input1TextBox.Text);
+                double num2 = Convert.ToDouble(Input2TextBox.Text);
+                double product = num1 * num2;
+
+                // display result to label (parsed)
+                labelResult.Content = product.ToString();
+            }
+            catch (Exception ex)
+            {
+                labelResult.Content = "Error" + ex.Message;
+            }
+        }
+
+        // logic for resut button click event
+        // clear textboxes and resultlabel
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Input1TextBox.Clear();
+            Input2TextBox.Clear();
+            labelResult.Content = string.Empty;
+
+            // sets focus back to first input textbox
+            Input1TextBox.Focus();
+        }
+
+        // logic for exit button click event
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // close app
+            // method in references
+            Application.Current.Shutdown();
+        }
     }
 }
